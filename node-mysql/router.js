845dd8e874ff07e31d7d6f500f038129eabe06a4
gaopeidi <gaopdsh@sinosoft.com.cn>
2021-06-25 17:14:30 +0800

    vue3函数理解

--- node-mysql/router.js
+++ node-mysql/router.js
@@ -1,3 +1,20 @@
+const resultObj = {
+  login: (method, params) => {
+    result.msg = "登录成功";
+    if (method !== "POST") {
+      result.code = "100"; //请求方式不对、
+      result.msg = "请求方式不对";
+    }
+    if (
+      params &&
+      (JSON.parse(params).userName !== "jy" ||
+        JSON.parse(params).userPsd != "123456")
+    ) {
+      result.code = "001"; // 用户或密码错误
+      result.msg = "用户或密码错误";
+    }
+  },
+};
 function route(pathname, method, params) {
   let api = pathname.split("/")[1];
   let result = {
--- vue3-mysql/src/views/Login.vue
+++ vue3-mysql/src/views/Login.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
     <div>
-      <el-form :model="form" label-width="120px">
-        <el-form-item label="用户名:">
+      <el-form
+        :model="form"
+        label-width="120px"
+        ref="formRef"
+        :rules="formRules"
+      >
+        <el-form-item label="用户名:" prop="userName">
           <el-input
             v-model="form.userName"
             placeholder="请输入用户名"
           ></el-input>
         </el-form-item>
-        <el-form-item label="密码:">
+        <el-form-item label="密码:" prop="userPsd">
           <el-input
             v-model="form.userPsd"
             placeholder="请输入密码"
@@ -20,43 +25,66 @@
     </div>
     <div>
       <el-button type="primary" @click="login">登录</el-button>
-      <el-button type="primary" @click="register"> 注册{{ msg }}</el-button>
+      <el-button type="primary" @click="register">注册</el-button>
     </div>
   </div>
 </template>
 <script>
-import { ref, getCurrentInstance } from 'vue'
+import { reactive, getCurrentInstance, ref, unref } from 'vue'
 import { loginAPI } from '../axios/apis'
 import { ElMessage } from 'element-plus'
 export default {
   setup() {
     //setup()函数在beforeCreate之前调用
+    const formRef = ref(null)
     let { proxy } = getCurrentInstance()
-    const msg = ref('')
-    const form = ref({
+    const form = reactive({
       userName: '',
       userPsd: '',
     })
-    console.log('ddddd', proxy)
-    async function login() {
-      const res = await loginAPI({
-        userName: proxy.form.userName,
-        userPsd: proxy.form.userPsd,
-      })
-      if (res.code === 0) {
-        proxy.$router.push({
-          path: '/home',
-        })
-      } else {
-        ElMessage.error(res.msg)
-      }
+    const formRules = {
+      userName: [
+        {
+          required: true,
+          message: '不能为空',
+          trigger: 'blur',
+        },
+      ],
+      userPsd: [
+        {
+          required: true,
+          message: '不能为空',
+          trigger: 'blur',
+        },
+      ],
     }
-    function register() {
+    const login = () => {
+      const reform = unref(formRef)
+      if (!reform) return
+      reform.validate(async (valid) => {
+        if (valid) {
+          const { userName, userPsd } = form
+          const res = await loginAPI({
+            userName: userName,
+            userPsd: userPsd,
+          })
+          if (res.code === 0) {
+            proxy.$router.push({
+              path: '/home',
+            })
+          } else {
+            ElMessage.error(res.msg)
+          }
+        }
+      })
+    }
+    const register = () => {
       console.log('register')
     }
     return {
-      msg,
       form,
+      formRules,
+      formRef,
       login,
       register,
     }
